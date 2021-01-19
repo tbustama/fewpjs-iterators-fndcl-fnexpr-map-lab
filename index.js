f71d24405aefc3166ea599300e8a7ea902951119
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-18 20:23:10 -0800

    commit tree 79b3169b7c74ce37e00e1f1aaa9f130b76505184

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Contutorialuctor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
   return tutorials
-}
+    .map((sent) => {
+      return sent.split(" ").map((word) => {
+        return word.slice(0, 1).toUpperCase() + word.slice(1);
+      });
+    })
+    .map((sent) => {
+      return sent.join(" ");
+    });
+};
